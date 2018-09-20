f56d0e1b864bfcd31cd53606e29014906739e75f
Martin Widmann <martin.widmann@russmedia.com>
2018-09-20 15:44:22 +0200

    samples of mocking async calls for tests

--- exercises/6-async-code/ItemView.spec.js
+++ exercises/6-async-code/ItemView.spec.js
@@ -1,5 +1,26 @@
-import ItemView from './ItemView'
+import { fetchItems } from './api'
+import ItemView from './ItemView.vue'
+import Item from './Item.vue'
+import { shallowMount } from '@vue/test-utils'
+import flushPromises from 'flush-promises'
 
-test('renders items resolved from fetchItems')
+jest.mock('./api')
 
-test('renders error if fetchItems rejects')
+test('renders items resolved from fetchItems', async () => {
+  const items = [{}, {}, {}]
+  fetchItems.mockResolvedValue(items)
+  const wrapper = shallowMount(ItemView)
+
+  await flushPromises();
+
+  expect(wrapper.findAll(Item)).toHaveLength(3)
+})
+
+test('renders error if fetchItems rejects', async () => {
+  fetchItems.mockRejectedValue(new Error('500'))
+  const wrapper = shallowMount(ItemView)
+
+  await flushPromises()
+
+  expect(wrapper.text()).toContain('Error loading items')
+})
--- exercises/6-async-code/api.spec.js
+++ exercises/6-async-code/api.spec.js
@@ -1,3 +1,18 @@
-test('resolves with data.items if /items returns 200')
+import { fetchItems } from './api'
+import axios from 'axios'
 
-test('rejects with error if /items returns 500')
+jest.mock('axios')
+
+test('resolves with data.items if /items returns 200', async () => {
+  expect.assertions(1)
+  const items = [{}, {}, {}]
+  axios.get.mockResolvedValue(items)
+  var returnedItems = await fetchItems()
+  expect(returnedItems).toBe(items)
+})
+
+test('rejects with error if /items returns 500', async () => {
+  expect.assertions(1)
+  axios.get.mockRejectedValue(new Error('500'))
+  expect(fetchItems()).rejects.toBeInstanceOf(Error)
+})
