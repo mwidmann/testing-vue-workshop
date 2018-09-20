00f9bc8f192204d0d27b10225ce3a6892dd93a58
Martin Widmann <martin.widmann@russmedia.com>
2018-09-20 12:20:12 +0200

    fixed indentation

--- exercises/2-components/Synopsis.spec.js
+++ exercises/2-components/Synopsis.spec.js
@@ -12,18 +12,18 @@ import Synopsis from './Synopsis.vue'
 //
 // https://vue-test-utils.vuejs.org/api/options.html#slots
 test('renders short slot, and hides long slot, initially', () => {
-    const long = 'Long slot'
-    const short = 'Short slot'
+  const long = 'Long slot'
+  const short = 'Short slot'
 
-    const wrapper = shallowMount(Synopsis, {
-        slots: {
-            long: `<p>${long}</p>`,
-            short: `<p>${short}</p>`
-        }
-    })
+  const wrapper = shallowMount(Synopsis, {
+    slots: {
+      long: `<p>${long}</p>`,
+      short: `<p>${short}</p>`
+    }
+  })
 
-    expect(wrapper.text()).toContain(short)
-    expect(wrapper.text()).not.toContain(long)
+  expect(wrapper.text()).toContain(short)
+  expect(wrapper.text()).not.toContain(long)
 })
 
 // You can use the Vue Test Utils find and trigger methods to trigger an event:
@@ -33,28 +33,28 @@ test('renders short slot, and hides long slot, initially', () => {
 //
 // https://vue-test-utils.vuejs.org/api/options.html#slots
 test('renders long slot, and hides short slot, when button is clicked', () => {
-    const long = 'Long slot'
-    const short = 'Short slot'
+  const long = 'Long slot'
+  const short = 'Short slot'
 
-    const wrapper = shallowMount(Synopsis, {
-        slots: {
-            long: `<p>${long}</p>`,
-            short: `<p>${short}</p>`
-        }
-    })
+  const wrapper = shallowMount(Synopsis, {
+    slots: {
+      long: `<p>${long}</p>`,
+      short: `<p>${short}</p>`
+    }
+  })
 
-    wrapper.find('button').trigger('click')
+  wrapper.find('button').trigger('click')
 
-    expect(wrapper.text()).not.toContain(short)
-    expect(wrapper.text()).toContain(long)
+  expect(wrapper.text()).not.toContain(short)
+  expect(wrapper.text()).toContain(long)
 })
 
 test('toggles "Show more/ show less" when button is clicked', () => {
-    const wrapper = shallowMount(Synopsis)
+  const wrapper = shallowMount(Synopsis)
 
-    expect(wrapper.text()).toContain('Show more')
+  expect(wrapper.text()).toContain('Show more')
 
-    wrapper.find('button').trigger('click')
+  wrapper.find('button').trigger('click')
 
-    expect(wrapper.text()).toContain('Show less')
+  expect(wrapper.text()).toContain('Show less')
 })
