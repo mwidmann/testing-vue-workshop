8b9a37a2925b5418e64dfe007d0a6d3656d018a3
Martin Widmann <martin.widmann@russmedia.com>
2018-09-20 12:08:40 +0200

    added component tests

--- exercises/2-components/List.spec.js
+++ exercises/2-components/List.spec.js
@@ -1,5 +1,20 @@
+import { shallowMount } from '@vue/test-utils'
 import List from './List.vue'
 
-test('renders "no items" if items prop is undefined')
+test('renders "no items" if items prop is undefined', () => {
+  const wrapper = shallowMount(List)
 
-test('renders text using each item in items prop array')
+  expect(wrapper.text()).toContain('No items')
+})
+
+test('renders text using each item in items prop array', () => {
+  const wrapper = shallowMount(List, {
+    propsData: {
+      items: ['item1', 'item2', 'item3']
+    }
+  })
+
+  expect(wrapper.text()).toContain('item1')
+  expect(wrapper.text()).toContain('item2')
+  expect(wrapper.text()).toContain('item3')
+})
--- exercises/2-components/Synopsis.spec.js
+++ exercises/2-components/Synopsis.spec.js
@@ -1,3 +1,4 @@
+import { shallowMount } from '@vue/test-utils'
 import Synopsis from './Synopsis.vue'
 
 // You can use the Vue Test Utils slots option to pass in a slot:
@@ -10,7 +11,20 @@ import Synopsis from './Synopsis.vue'
 // })
 //
 // https://vue-test-utils.vuejs.org/api/options.html#slots
-test('renders short slot, and hides long slot, initially')
+test('renders short slot, and hides long slot, initially', () => {
+    const long = 'Long slot'
+    const short = 'Short slot'
+
+    const wrapper = shallowMount(Synopsis, {
+        slots: {
+            long: `<p>${long}</p>`,
+            short: `<p>${short}</p>`
+        }
+    })
+
+    expect(wrapper.text()).toContain(short)
+    expect(wrapper.text()).not.toContain(long)
+})
 
 // You can use the Vue Test Utils find and trigger methods to trigger an event:
 //
@@ -18,6 +32,29 @@ test('renders short slot, and hides long slot, initially')
 // wrapper.find('button').trigger('click')
 //
 // https://vue-test-utils.vuejs.org/api/options.html#slots
-test('renders long slot, and hides short slot, when button is clicked')
+test('renders long slot, and hides short slot, when button is clicked', () => {
+    const long = 'Long slot'
+    const short = 'Short slot'
 
-test('toggles "Show more/ show less" when button is clicked')
+    const wrapper = shallowMount(Synopsis, {
+        slots: {
+            long: `<p>${long}</p>`,
+            short: `<p>${short}</p>`
+        }
+    })
+
+    wrapper.find('button').trigger('click')
+
+    expect(wrapper.text()).not.toContain(short)
+    expect(wrapper.text()).toContain(long)
+})
+
+test('toggles "Show more/ show less" when button is clicked', () => {
+    const wrapper = shallowMount(Synopsis)
+
+    expect(wrapper.text()).toContain('Show more')
+
+    wrapper.find('button').trigger('click')
+
+    expect(wrapper.text()).toContain('Show less')
+})
