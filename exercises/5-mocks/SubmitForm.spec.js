f7cb78b4f6208566ad30e9755068fb94af82f173
Martin Widmann <martin.widmann@russmedia.com>
2018-09-20 14:06:57 +0200

    mocking functions

--- exercises/5-mocks/SubmitForm.spec.js
+++ exercises/5-mocks/SubmitForm.spec.js
@@ -1,4 +1,15 @@
 import { shallowMount } from '@vue/test-utils'
 import SubmitForm from './SubmitForm.vue'
 
-test('calls onSubmit with value of input when form is submitted')
+test('calls onSubmit with value of input when form is submitted', () => {  const onSubmit = jest.fn()
+  const wrapper = shallowMount(SubmitForm, {
+    propsData: {
+      onSubmit
+    }
+  })
+
+  wrapper.find('[type="text"]').setValue('Edd')
+  wrapper.find('[type="submit"]').trigger('submit')
+
+  expect(onSubmit).toHaveBeenCalledWith('Edd')
+})
