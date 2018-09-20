d82779287f86aba990102a2e824bcab26709f19a
Martin Widmann <martin.widmann@russmedia.com>
2018-09-20 12:59:14 +0200

    vue events testing with .emitted

--- exercises/3-vue-events/Modal.spec.js
+++ exercises/3-vue-events/Modal.spec.js
@@ -1,1 +1,9 @@
-test('emits on-close event when close button is clicked')
+import { shallowMount } from '@vue/test-utils'
+import Modal from './Modal.vue'
+
+test('emits on-close event when close button is clicked', () => {
+  const wrapper = shallowMount(Modal)
+  expect(wrapper.emitted('close-modal')).toBeUndefined()
+  wrapper.find('[aria-label="close"]').trigger('click')
+  expect(wrapper.emitted('close-modal')).toHaveLength(1)
+})
